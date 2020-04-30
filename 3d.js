ed62a44f938bfdda610adfe454b1e8ef4731f195
“Stephen <user@MultivAC.localdomain>
2020-04-30 16:19:26 -0600

    added crappy functions to 3d.js, import, and begining to buttonPusher.js

--- 3d.js
+++ 3d.js
@@ -1,16 +1,26 @@
+//just literally grab everything from three's modules
+//import { Bone } from './three/build/three.module.js';
+//import * as THREE from './three/build/three.module.js';
 import { VRButton } from './three/examples/jsm/webxr/CustomVRButton.js';
+//import { XRControllerModelFactory } from './three/examples/jsm/webxr/XRControllerModelFactory.js';
+//import {createController } from './buttonPusher.js'
 
 var scene, camera, renderer;
+
+var controller1, controller2;
+var controllerGrip1, controllerGrip2;
+var cameraFixture;
+var raycaster, intersected = [];
+
 function init(){
+    cameraFixture = new Group();
     renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.xr.enabled = true;
     //renderer.setClearColor("#858585");
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild(VRButton.createButton(renderer));
-
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-    camera.position.set(0, 20, 30);
-
+    
+    
     document.body.appendChild( renderer.domElement );
 
     scene = new THREE.Scene();{
@@ -26,6 +36,14 @@ function init(){
         scene.background = texture;
       }
 
+      //making the head a referenceable location
+    
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    cameraFixture.add(camera);
+    cameraFixture.position.set(2, 2, -1);
+    scene.add( cameraFixture );
+
+
     var controls = new THREE.OrbitControls(camera, renderer.domElement);
     controls.enableDampening = true;
     controls.campingFactor = 0.25;
@@ -34,7 +52,7 @@ function init(){
     var keyLight = new THREE.DirectionalLight(0xffffff, 1.0);
     keyLight.position.set(-60, 0, 60);
 
-    var fillLight = new THREE.DirectionalLight(0xffffff, 0.75);
+    var fillLight = new THREE.DirectionalLight(0xffffff, 0.50);
     fillLight.position.set(60, 0, 60);
 
     var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
@@ -49,20 +67,47 @@ function init(){
         camera.aspect = window.innerWidth / window.innerHeight;
 
         //camera.updatePojectionMatrix();
+        
     })
+
 }
 
 var geometry, material, shape;
 var shapes = new Array;
-function makeShape(type){
+function makeShape(type, xPos=0, yPos=0, zPos=0){
     if(type=="sphere")
         geometry = new THREE.SphereGeometry();
     if(type=="cube")
-        geometry = new THREE.CubeGeometry(3, 3, 3);
-    material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true} );
+        geometry = new THREE.CubeGeometry(1, 1, 1);
+    material = new THREE.MeshBasicMaterial( { color: 0x00ff00,flatShading: true, wireframe: false} );
     shape = new THREE.Mesh( geometry, material );
+    shape.geometry.translate(xPos, yPos, zPos);
     shapes.push(shape);
     scene.add( shape );
+}
+function createController( controllerId ) {
+    // RENDER CONTROLLER
+    const controller = renderer.vr.getController( controllerId );
+    const cylinderGeometry = new THREE.CylinderGeometry( 0.025, 0.025, 1, 32 );
+    const cylinderMaterial = new THREE.MeshPhongMaterial( {color: 0xffff00} );
+    const cylinder = new THREE.Mesh( cylinderGeometry, cylinderMaterial );
+    cylinder.geometry.translate( 0, 0.5, 0 );
+    cylinder.rotateX( - 0.25 * Math.PI );
+    controller.add( cylinder );
+    cameraFixture.add( controller );
+    // TRIGGER
+    controller.addEventListener( 'selectstart', () => { cylinderMaterial.color.set( 0xff0000 ) } );
+    controller.addEventListener( 'selectend', () => { cylinderMaterial.color.set( 0xffff00 ) } );
+    var geometry = new THREE.BufferGeometry().setFromPoints( [ new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, - 1 ) ] );
+
+    var line = new THREE.Line( geometry );
+    line.name = 'line';
+    line.scale.z = 5;
+
+    // controller1.add( line.clone() );
+    // controller2.add( line.clone() );
+
+    // raycaster = new THREE.Raycaster();
 }
 
 function loadObj(path){
@@ -105,9 +150,11 @@ function animate() {
 
 function main(){
     init();
-    makeShape("sphere");
-    makeShape("cube");
+    makeShape("sphere", 0, 0, 0);
+    makeShape("cube",0,0,0);
     loadObj('models/human.obj');
+    createController(0);
+    createController(1);
     animate();
 }
 
